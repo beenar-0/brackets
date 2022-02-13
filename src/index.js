7cb817923c376681ca8036239707ebb0e1d760a2
beenar-0 <dncastledn@gmail.com>
2022-02-14 01:10:08 +0300

    feat: added kostyl' solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false
+    let count = 0
+    str = str.split('')
+    while (count <= 40) {
+        function search() {
+            str.forEach((strItem, strIndex)=>{
+                bracketsConfig.forEach((configItem)=>{
+                    if (strItem === configItem[0]) {
+                        if (str[strIndex + 1] === configItem[1]) {
+                            str.splice(strIndex, 2)
+                        }
+                    }
+                })
+            })
+        }
+        search()
+        count++
+    }
+    return str.length <= 0;
 }
